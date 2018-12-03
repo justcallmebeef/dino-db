56e05196c6eb4f31f35032d779a72b580546a39a
justcallmebeef <bethanymitch10@gmail.com>
2018-12-03 15:20:12 -0700

    added a length on description in the schema

--- migrations/20181203145107_listings.js
+++ migrations/20181203145107_listings.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('listings', (listing) => {
     listing.increments('id')
     listing.string('title')
-    listing.string('description')
+    listing.string('description', [500])
     listing.string('pay')
     listing.string('interested')
   })
