64e97e20d6764e5bbf85b251e7992b43d693a058
justcallmebeef <bethanymitch10@gmail.com>
2018-12-03 15:09:52 -0700

    updated spacing in seed data

--- seeds/01_listings.js
+++ seeds/01_listings.js
@@ -6,28 +6,28 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('listings').insert([   
         {
-        title:"Demolitions Manager",
-        description:"Please destroy things for us.",
-        pay:"$50,000 per year",
-        interested:["T-Rex"]
+          title:"Demolitions Manager",
+          description:"Please destroy things for us.",
+          pay:"$50,000 per year",
+          interested:["T-Rex"]
         },
         {
-        title:"Tree Trimming Specialist",
-        description:"Responsible for keeping trees and bushes well trimmed and away from structures. Must be able to dispose of trimmings.",
-        pay:"$23.50 per hour",
-        interested:["Utahraptor",""]
+          title:"Tree Trimming Specialist",
+          description:"Responsible for keeping trees and bushes well trimmed and away from structures. Must be able to dispose of trimmings.",
+          pay:"$23.50 per hour",
+          interested:["Utahraptor",""]
         },
         {
-        title:"Dog Walker",
-        description:"Looking for a dog lover to take our furry friends out three times a day.",
-        pay:"$15 per hour",
-        interested:["Dromiceiomimus"]
+          title:"Dog Walker",
+          description:"Looking for a dog lover to take our furry friends out three times a day.",
+          pay:"$15 per hour",
+          interested:["Dromiceiomimus"]
         },
         {
-        title:"Philosopher",
-        description:"An ink sees a colombia as a boxlike composition. We can assume that any instance of a currency can be construed as a charming macaroni. If this was somewhat unclear, barish lyrics show us how himalayans can be fathers. A statement can hardly be considered a backless denim without also being a feeling.",
-        pay:"24% equity",
-        interested:["T-Rex","Utahraptor"]
+          title:"Philosopher",
+          description:"An ink sees a colombia as a boxlike composition. We can assume that any instance of a currency can be construed as a charming macaroni. If this was somewhat unclear, barish lyrics show us how himalayans can be fathers. A statement can hardly be considered a backless denim without also being a feeling.",
+          pay:"24% equity",
+          interested:["T-Rex","Utahraptor"]
       }]
     );
   });
